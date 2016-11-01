fa8b531a7a634e94ef4245e924a23f06cddc6786
Unknown <david.zimmermann@zu.de>
2016-11-02 00:32:27 +0100

    Added Binomial Tree and Black-Scholes

--- R/functions.R
+++ R/functions.R
@@ -187,3 +187,96 @@ sensitivityWrapper <- function(input_params, dat) {
   
 }
 
+
+###############
+# Binomial Tree
+###############
+get_edges <- function(n_steps) {
+  res <- lapply(0:n_steps, function(t) {
+    x_start <- t - 1
+    x_end <- t
+    y_end <- seq(from = t, to = -t, by = -2)
+    
+    y_start <- y_end - 1
+    
+    y_start <- y_start[-length(y_start)]
+    
+    if (t > 0) {
+      values <- data.table(xs = x_start,
+                           xe = x_end,
+                           ys = rep(y_start, length(y_end)), 
+                           ye = rep(y_end, length(y_start)))[abs(ys - ye) < 2]
+    } else {
+      nu <- numeric(0)
+      values <- data.table(xs = nu,
+                           xe = nu,
+                           ys = nu,
+                           ye = nu)
+    }
+    
+    return(values)
+  })
+  return(rbindlist(res))
+}
+get_nodes <- function(n_steps) {
+  res <- lapply(0:n_steps, function(t) {
+    x <- t
+    y <- seq(from = t, to = -t, by = -2)
+    
+    return(data.table(x = x, y = y))
+  })
+  res2 <- rbindlist(res)
+  
+  return(res2)
+}
+create_tree <- function(n_steps, type = "call", s_0 = 100, tick = 10, k = 100, rf = 0.1) {
+  
+  edges <- get_edges(n_steps)
+  nodes <- get_nodes(n_steps)
+  
+  
+  nodes[, s_t := y*tick + s_0]
+  
+  if (type == "call") {
+    nodes[x == max(x), c_t := ifelse(s_t > k, s_t - k, 0)]
+  } else if (type == "put") {
+    nodes[x == max(x), c_t := ifelse(s_t < k, k - s_t, 0)]
+  } else {
+    stop("Unknown type, only 'call'/'put' allowed!")
+  }
+  
+  nodes[, ':=' (delta = NA, b = NA)]
+  
+  for (lvl in (max(nodes$x) - 1):0) {
+    tmp <- nodes[x == lvl]
+    
+    nodes_up <- nodes[x == lvl + 1][, .(y = y - 1, c_u = c_t, s_u = s_t)]
+    nodes_down <- nodes[x == lvl + 1][, .(y = y + 1, c_d = c_t, s_d = s_t)]
+    
+    tmp <- merge(tmp, nodes_up, by = "y", all.x = T)
+    tmp <- merge(tmp, nodes_down, by = "y", all.x = T)
+    tmp[, delta := (c_u - c_d) / (s_u - s_d)]
+    tmp[, b := (c_d - s_d * delta) / (1 + rf)]
+    tmp[, c_t := s_t * delta + b]
+    nodes <- rbindlist(list(tmp[, .(x, y, s_t, c_t, delta, b)],
+                            nodes[x != lvl,]))
+  }
+  
+  nodes[, label := paste("S[t] = ", s_t, 
+                         "\nDelta = ", round(delta, 4),
+                         "\nB = ", round(b, 4),
+                         "\nC[t] = ", round(c_t, 4))]
+  
+  ggplot() + 
+    theme_void() +
+    geom_segment(data = edges, aes(x = xs, xend = xe, y = ys, yend = ye)) + 
+    geom_label(data = nodes, 
+               aes(x = x, y = y, label = label), fill = "white") + # parse = T
+    geom_label(data = data.table(x = 0:max(nodes$x), y = max(nodes$y) + 1, 
+                                 lab = paste0("t = ", 0:max(nodes$x))),
+               aes(x = x, y = y, label = lab), fill = "white") +
+    scale_y_continuous(limits = c(-1,1) + range(nodes$y))
+  
+}
+
+
--- app.R
+++ app.R
@@ -19,7 +19,7 @@ library(shiny)
 source("R/functions.R")
 
 # uncomment this if the text-files should be recompiled
-# rmdfiles <- c("files/about.rmd", "files/text_intro_options.rmd")
+# rmdfiles <- c("files/about.rmd", "files/text_intro_options.rmd", "files/text_intro_valuation.rmd")
 # sapply(rmdfiles, knit, quiet = T)
 # sapply(rmdfiles, render, quiet = T)
 
@@ -27,11 +27,11 @@ source("R/functions.R")
 # UI #
 ########################################################################
 
-# calc_ui for the calculation-tab
-calc_ui <- function() {
+# payoff_ui for the calculation-tab
+payoff_ui <- function() {
   sidebarLayout(
     sidebarPanel = sidebarPanel(
-      h3("Add Option to Basket"),
+      h3("Option Basket Input"),
       radioButtons("I_basket_dir", "Position", 
                    list("Long" = 1, "Short" = 2), 
                    selected = 1, inline = T),
@@ -51,7 +51,9 @@ calc_ui <- function() {
     ),
     mainPanel = mainPanel(
                tabsetPanel("asd",
-                           tabPanel("Payoff-Diagramm",
+                           tabPanel("Theory",
+                                    htmlOutput("payoff_theory")),
+                           tabPanel("Figure",
                                     plotOutput("p_payoffs")
                            ),
                            tabPanel("Data",
@@ -67,39 +69,41 @@ calc_ui <- function() {
 greek_ui <- function() {
   sidebarLayout(
     sidebarPanel = sidebarPanel(
-      h3("Change Option"),
-      radioButtons("I_single_dir", "Position", 
+      h3("Financial Greek Input"),
+      radioButtons("I_greek_dir", "Position", 
                    list("Long" = 1, "Short" = 2), 
                    selected = 1, inline = T),
-      radioButtons("I_single_type", "Option",
+      radioButtons("I_greek_type", "Option",
                    list("Call" = 1, "Put" = 2),
                    selected = 1, inline = T),
-      radioButtons("I_single_eu_am", "Option Type",
+      radioButtons("I_greek_eu_am", "Option Type",
                    list("European" = 1, "American" = 2), inline = T),
-      sliderInput("I_single_strike", "Strike Price (Option)", value = 100,
+      sliderInput("I_greek_strike", "Strike Price (in $)", value = 100,
                   min = 1, max = 250),
-      sliderInput("I_single_value_underlying", "Current Value Underlying", 
+      sliderInput("I_greek_value_underlying", "Current Value Underlying (in $)", 
                   value = 100,
                   min = 1, max = 250),
-      dateInput("I_single_maturity", "Maturity", value = Sys.Date() + 31,
-                min = Sys.Date() + 1, max = Sys.Date() + 365 * 2),
-      sliderInput("I_single_dvd_yield", "Dividend Yield (in %)", value = 5,
+      sliderInput("I_greek_maturity", "Maturity (in years)", value = 1,
+                  min = 0, max = 5, step = 0.1),
+      # dateInput("I_greek_maturity", "Maturity", value = Sys.Date() + 31,
+      #           min = Sys.Date() + 1, max = Sys.Date() + 365 * 2),
+      sliderInput("I_greek_dvd_yield", "Dividend Yield (in %)", value = 5,
                   min = 0, max = 20),
-      sliderInput("I_single_rf", "Risk-Free Rate (in %)", value = 1,
+      sliderInput("I_greek_rf", "Risk-Free Rate (in %)", value = 1,
                   min = 0, max = 20),
-      sliderInput("I_single_vola", "Volatility (in %)", value = 1,
+      sliderInput("I_greek_vola", "Volatility (in %)", value = 1,
                   min = 0, max = 20)
     ),
     mainPanel = mainPanel(
       tabsetPanel("",
                   tabPanel("Theory",
                            DT::dataTableOutput("greek_theory")
-                           ),
+                  ),
                   tabPanel("Text Result",
                            verbatimTextOutput("text_single_result")
-                           ),
-                  tabPanel("The Greeks",
-                           radioButtons("I_single_greek", "Variable",
+                  ),
+                  tabPanel("The Greeks ",
+                           radioButtons("I_greek_var", "Variable",
                                         list("Underlying" = "underlying",
                                              "Strike" = "strike",
                                              "Dividend Yield" = "dvd_yield",
@@ -107,21 +111,83 @@ greek_ui <- function() {
                                              "Maturity" = "maturity",
                                              "Volatility" = "vola"),
                                         inline = T),
-                           plotOutput("plot_greeks")
-                           )
+                           plotOutput("plot_greeks"),
+                           "Note: the greeks refer mostly to the cases where the variable is the underlying."
                   )
+      )
     )
+  )
+}
+
+# price_ui for the display of the greeks
+price_ui <- function() {
+  fluidPage(tabsetPanel("",
+                        tabPanel("Theory",
+                                 htmlOutput("price_theory")
+                        ),
+                        tabPanel("Binomial Model",
+                                 sidebarLayout(
+                                     sidebarPanel = sidebarPanel(
+                                       h3("Binomial Input"),
+                                       numericInput("I_price_n_steps", "Number of Steps",
+                                                    min = 1, max = 10, value = 3),
+                                       radioButtons("I_price_type", "Option",
+                                                    list("Call" = 1, "Put" = 2),
+                                                    selected = 1, inline = T),
+                                       sliderInput("I_price_strike", "Strike Price (in $)", value = 100,
+                                                   min = 1, max = 250),
+                                       sliderInput("I_price_value_underlying", "Current Value Underlying (in $)",
+                                                   value = 100,
+                                                   min = 1, max = 250),
+                                       sliderInput("I_price_tick", "Tick (in $)", value = 10,
+                                                   min = 0, max = 50),
+                                       sliderInput("I_price_rf", "Risk-Free Rate (in %)", value = 1,
+                                                   min = 0, max = 20)
+                                     ),
+                                     mainPanel = mainPanel(
+                                       plotOutput("price_binomial")
+                                     )
+                                   )
+                        ),
+                        tabPanel("Black-Scholes Model",
+                                 sidebarLayout(
+                                   sidebarPanel = sidebarPanel(
+                                     h3("Black-Scholes Input"),
+                                     radioButtons("I_bs_type", "Option",
+                                                  list("Call" = 1, "Put" = 2),
+                                                  selected = 1, inline = T),
+                                     sliderInput("I_bs_strike", "Strike Price (in $)", value = 100,
+                                                 min = 1, max = 250),
+                                     sliderInput("I_bs_value_underlying", "Current Value Underlying (in $)", 
+                                                 value = 100,
+                                                 min = 1, max = 250),
+                                     sliderInput("I_bs_maturity", "Maturity (in years)", value = 1,
+                                                 min = 0, max = 5, step = 0.1),
+                                     sliderInput("I_bs_rf", "Risk-Free Rate (in %)", value = 1,
+                                                 min = 0, max = 20, step = 0.1),
+                                     sliderInput("I_bs_vola", "Volatility (in %)", value = 1,
+                                                 min = 0, max = 20, step = 0.1)
+                                   ),
+                                   mainPanel = mainPanel(
+                                     uiOutput("price_bs")
+                                   )
+                                 )
+                        )
+            )
   )
 }
 
 # main_file for the main-tabs 
 main_ui <- shinyUI(
   navbarPage("Options 101",
-             tabPanel("Theory", #"asd"),
-                      htmlOutput("theory")),
+             #tabPanel("Theory", #"asd"),
+            #          htmlOutput("theory")),
              #includeHTML("files/text_intro_options.html")),
-             tabPanel("Option Calculations",
-                      calc_ui()
+             tabPanel("Payoff",
+                      payoff_ui()
+             ),   
+             tabPanel("Valuation",
+                      price_ui()
              ),
              tabPanel("Price and the Greeks",
                       greek_ui()
@@ -141,7 +207,8 @@ main_ui <- shinyUI(
 
 server_fun <- function(input, output, session) {
   # init ----
-  output$theory <- renderUI({includeHTML("files/text_intro_options.html")})
+  output$payoff_theory <- renderUI({includeHTML("files/text_intro_options.html")})
+  output$price_theory <- renderUI({includeHTML("files/text_intro_valuation.html")})
   empty_dt_positions()
   render_plot1(output = output)
   render_positions_table(output)
@@ -211,18 +278,19 @@ server_fun <- function(input, output, session) {
   })
   
   # Single Options ----
-  option <- reactive(data.table(type = ifelse(input$I_single_type == 1, "call", 
+  option <- reactive(data.table(type = ifelse(input$I_greek_type == 1, "call", 
                                               "put"),
-                                dir = ifelse(input$I_single_dir == 1, "long", 
+                                dir = ifelse(input$I_greek_dir == 1, "long", 
                                              "short"),
-                                strike = input$I_single_strike,
-                                underlying = input$I_single_value_underlying,
-                                maturity = as.numeric(input$I_single_maturity -
-                                                        Sys.Date()) / 365,
-                                dvd_yield = input$I_single_dvd_yield / 100,
-                                rf = input$I_single_rf / 100,
-                                vola = input$I_single_vola / 100,
-                                eu_am = ifelse(input$I_single_eu_am == 1,
+                                strike = input$I_greek_strike,
+                                underlying = input$I_greek_value_underlying,
+                                maturity = input$I_greek_maturity,
+                                # as.numeric(input$I_greek_maturity -
+                                #                      Sys.Date()) / 365,
+                                dvd_yield = input$I_greek_dvd_yield / 100,
+                                rf = input$I_greek_rf / 100,
+                                vola = input$I_greek_vola / 100,
+                                eu_am = ifelse(input$I_greek_eu_am == 1,
                                                "European", "American")))
   
   option_result <- reactive({
@@ -251,7 +319,7 @@ server_fun <- function(input, output, session) {
   greek_plot_data <- reactive({
     dat <- option()
     
-    var <- input$I_single_greek
+    var <- input$I_greek_var
     
     val <- dat[, var, with = F] %>% as.numeric()
     
@@ -274,12 +342,12 @@ server_fun <- function(input, output, session) {
   })
   
   interpretation <-  c("Value of the option",
-                       "Sensitivity of the option value for a change in the underlying",
-                       "Sensitivity of the option delta for a change in the underlying",
-                       "Sensitivity of the option value for a change in the underlying's volatility",
-                       "Sensitivity of the option value for a change in t, the remaining time to maturity",
-                       "Sensitivity of the option value for a change in the risk-free interest rate",
-                       "Sensitivity of the option value for a change in the dividend yield")
+                       "Sensitivity of the option value to a change in the underlying",
+                       "Sensitivity of the option delta to a change in the underlying",
+                       "Sensitivity of the option value to a change in the underlying's volatility",
+                       "Sensitivity of the option value to a change in t, the remaining time to maturity",
+                       "Sensitivity of the option value to a change in the risk-free interest rate",
+                       "Sensitivity of the option value to a change in the dividend yield")
   
   df <- data.frame(greek = c("value", "delta", "gamma", "vega", "theta", "rho", "dividendRho"),
                    interpretation = interpretation)
@@ -289,6 +357,100 @@ server_fun <- function(input, output, session) {
   output$greek_theory <- DT::renderDataTable(df_dt)
   
   output$text_single_result <- renderPrint(option_result())
+  
+  
+  # Prices & Valuation
+  
+  output$price_binomial <- renderPlot({
+    # cat("n_steps: ", input$I_price_n_steps,
+    #     "\ntype: ", input$I_price_type,
+    #     "\ns_0: ", input$I_price_value_underlying,
+    #     "\ntick: ", input$I_price_tick,
+    #     "\nk: ", input$I_price_strike,
+    #     "\nrf: ", input$I_price_rf,"\n")
+    create_tree(n_steps = input$I_price_n_steps,
+                type = ifelse(input$I_price_type == 1, "call", "put"),
+                s_0 = input$I_price_value_underlying, 
+                tick = input$I_price_tick, 
+                k = input$I_price_strike, 
+                rf = input$I_price_rf/100)
+  }, height = 700)
+  
+  bs_data <- reactive({
+    list(
+      type = input$I_bs_type,
+      s0 = input$I_bs_value_underlying,
+      k = input$I_bs_strike,
+      r = input$I_bs_rf/100,
+      ti = input$I_bs_maturity,
+      s = input$I_bs_vola/100
+    )
+  })
+  
+  output$price_bs <- renderUI({
+    text1 <- "The Black-Scholes formula predicts an option price for a European call option to be equal to
+$$ C = S_0 N(d_1) - Ke^{-rt} N(d_2) $$
+with
+$$ d_1 = \\frac{ln(S_o/Ke^{-rT})}{\\sigma \\sqrt{T}} $$
+and 
+$$ d_2 = d_1 - \\sigma \\sqrt{T}. $$
+
+The put option is priced using the put-call parity and is expected to be worth
+
+$$ P = Ke^{-rT} N(-d_2) - S_0N(-d_1). $$
+
+
+The result of the inputs from the left side are:
+    "
+    
+    text2_call <- "$$ C = %.0f*N(%.03f) - %.0f e^{-%.03f * %.01f} * N(%.03f) = %.03f $$
+with 
+$$ d_1 = \\frac{ln(%.0f/%.0fe^{-%.03f*%.01f})}{%.03f\\sqrt{%.01f}} = %.03f, $$
+
+$$ d_2 = %.03f - %.03f \\sqrt{%.01f} = %0.3f, $$
+
+$$ N(d_1) = N(%.03f) = %.03f, $$
+and
+$$ N(d_2) = N(%.03f) = %.03f, $$
+    "
+    
+    text2_put <- "$$ P = %.0fe^{%.03f*%.01f} * N(-%.03f) - %.0f * N(-%.03f) = %.03f $$
+with 
+$$ d_1 = \\frac{ln(%.0f/%.0fe^{%.03f*%.01f})}{%.03f\\sqrt{%.01f}} = %.03f $$
+
+$$ d_2 = %.03f - %.03f \\sqrt{%.01f} = %0.3f, $$
+
+$$ N(-d1) = N(%.03f) = %.03f, $$
+and
+$$ N(-d2) = N(%.03f) = %.03f, $$
+    "
+    
+    s0 <- bs_data()$s0
+    k <- bs_data()$k
+    r <- bs_data()$r
+    ti <- bs_data()$ti
+    s <- bs_data()$s
+    
+    d1 <- (log(s0/(k*exp(-r*ti)))) / (s*sqrt(ti))
+    d2 <-  d1 - s*sqrt(ti)
+    n_d1 <-  pnorm(d1)
+    n_d2 <- pnorm(d2)
+    n_d1_minus <- pnorm(-d1)
+    n_d2_minus <- pnorm(-d2)
+    C <- s0*n_d1 - k * exp(-r*ti)*n_d2
+    P <- k*exp(-r*ti)*n_d2_minus - s0*n_d1_minus
+    
+    if (bs_data()$type == 1) {
+      withMathJax(
+        sprintf(paste0(text1, text2_call), s0, d1, k, r, ti, d2, C, s0, k, r, ti, 
+                s, ti, d1, d1, s, ti, d2, d1, n_d1, d2, n_d2))
+    } else {
+      withMathJax(
+        sprintf(paste0(text1, text2_put), k, -r, ti, d2, s0, d1, P, s0, k, -r, ti,
+                s, ti, d1, d1, s, ti, d2, -d1, n_d1_minus, -d2, n_d2_minus))
+    }
+  })
+  
 }
 
 ########################################################################
